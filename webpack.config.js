16e19305848b23b9772f484ceb3c556e3f876b4a
KYE <kye@KYEdeMacBook-Pro-10.local>
2019-03-12 15:21:39 +0800

    feat: webpack-dev-server简易使用

--- webpack.config.js
+++ webpack.config.js
@@ -9,5 +9,11 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'build'),
     filename: '[name]-[contenthash].js'
+  },
+  devServer: {
+    contentBase: './build', // 设置服务器访问的基本目录
+    host: 'localhost',
+    port: '8081',
+    open: true // 自动打开页面
   }
 }
