ab5bc983388e017e15c5c02b81e2e03bb06c6c3e
KYE <kye@KYEdeMacBook-Pro-10.local>
2019-03-12 14:35:55 +0800

    feat: 单入口多文件

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: ['./src/index.js', './src/index2.js'],
   output: {
     path: path.resolve(__dirname,'build'),
     filename: '[name]-[contenthash].js'
